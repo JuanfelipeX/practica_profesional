f1dbaadd104d79adfaf18014928c454550e00e32
Juan Felipe Jimenez P <pipesjimenez6@gmail.com>
2023-09-26 18:44:30 -0500

    ng build

--- src/app/components/inicio/inicio.component.ts
+++ src/app/components/inicio/inicio.component.ts
@@ -82,7 +82,7 @@ export class InicioComponent implements OnInit {
   }
 
   abrirModal() {
-    const modalRef = this.modalService.open(FrameworkComponent, {size: 'xl', windowClass: 'modal-xxl'});
+    const modalRef = this.modalService.open(FrameworkComponent, { size: 'xl', windowClass: 'modal-xxl' });
     // Puedes pasar datos al componente del modal utilizando la siguiente línea
     // modalRef.componentInstance.data = tuData;
   }
